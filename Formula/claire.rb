116e9f32c161f41a9bb2cf8c22bd62a9d6ad107a
netuoso <andrew@boonedevelopment.com>
2025-03-07 22:56:09 -0600

    Updated brew formula

--- Formula/claire.rb
+++ Formula/claire.rb
@@ -5,26 +5,15 @@
 class Claire < Formula
   desc "CLaiRE CLI for use with CLaiRE SaaS API"
   homepage "https://claire.ac"
-  url "https://github.com/CLaiRE-AC/CLaiRE/releases/download/v0.1.0/claire-v0.1.0-d4133e7a-linux-x64.tar.xz"
-  sha256 "39dcabef10ada028ea83e45a4bc3f3cb6512d137b263744a6717c76f2795ed3a"
+  url "https://github.com/CLaiRE-AC/CLaiRE/releases/download/v0.1.0/claire-v0.1.0-d4133e7a-x64.pkg"
+  sha256 "c2f7e94fad9069bbc0fad0f6546dbdb01842a0ce0fdc466c1ea2a2b54b4c2dd9"
   version "0.1.0"
   version_scheme 1
 
   on_macos do
     on_arm do
-      url "https://github.com/CLaiRE-AC/CLaiRE/releases/download/v0.1.0/claire-v0.1.0-d4133e7a-linux-arm64.tar.xz"
-      sha256 "557119456c5079d88e9cff587e345a695fdc1d7ef47659806dc89f195cfb0f61"
-    end
-  end
-
-  on_linux do
-    on_intel do
-      url "https://github.com/CLaiRE-AC/CLaiRE/releases/download/v0.1.0/claire-v0.1.0-d4133e7a-linux-x64.tar.xz"
-      sha256 "39dcabef10ada028ea83e45a4bc3f3cb6512d137b263744a6717c76f2795ed3a"
-    end
-    on_arm do
-      url "https://github.com/CLaiRE-AC/CLaiRE/releases/download/v0.1.0/claire-v0.1.0-d4133e7a-linux-arm.tar.xz"
-      sha256 "2635a98b7d9f1ac0a7ae76e58a3e6714631061e04b5370027915fe2f3daf7819"
+      url "https://github.com/CLaiRE-AC/CLaiRE/releases/download/v0.1.0/claire-v0.1.0-d4133e7a-arm64.pkg"
+      sha256 "195225ea700a385b1c04d6921f1964e810946b4a541b91ce5477dfd0e1aaff13"
     end
   end
 
